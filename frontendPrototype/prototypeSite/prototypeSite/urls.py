e17c8fede825584ca7c424218a1868a8c918bccc
Daniel Norbert Meister <d.meister@ncl.ac.uk>
2023-06-21 16:33:17 +0200

    little finishing touches, upload button continues to not function

--- frontendPrototype/prototypeSite/prototypeSite/urls.py
+++ frontendPrototype/prototypeSite/prototypeSite/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+# TODO: fix import error
 from prototypeApp import views
 
 urlpatterns = [
