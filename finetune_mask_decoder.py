7791981d3c68f19f60fda5cf0d3ff87c5242d7fe
swezel <stefan.wezel@maddox.ai>
2023-06-27 14:20:59 +0200

    pass paths as agrs

--- finetune_mask_decoder.py
+++ finetune_mask_decoder.py
@@ -4,7 +4,7 @@ import torchvision
 import torch.nn.functional as F
 from torch.nn.functional import threshold, normalize
 from torchvision.models._utils import IntermediateLayerGetter
-import albumentations as A
+# import albumentations as A
 import argparse
 
 import utils
@@ -24,10 +24,12 @@ if __name__ == "__main__":
 
 
     parser.add_argument('--data_src', default="/mnt/qb/work/akata/aoq833/sam_finetuning_data")
-    parser.add_argument('--checkpoint_path', default="/mnt/qb/work/akata/aoq833/checkpoints/sam.pth")      # option that takes a value
+    parser.add_argument('--checkpoint_path', default="/mnt/qb/work/akata/aoq833/checkpoints")      # option that takes a value
+    parser.add_argument('--checkpoint_loadname', default="sam2.pth")      # option that takes a value
+    parser.add_argument('--checkpoint_savename', default="sam3.pth")      # option that takes a value
     parser.add_argument('--training_sets', type=list, default=['TCGA_CS_4941_19960909'])      # option that takes a value
     parser.add_argument('--test_sets', type=list, default=['TCGA_HT_A61B_19991127'])      # option that takes a value
-    parser.add_argument('--num_epochs', type=int, default=10)      # option that takes a value
+    parser.add_argument('--num_epochs', type=int, default=20)      # option that takes a value
     parser.add_argument('--batch_size', type=int, default=1)      # option that takes a value
     parser.add_argument('--lr', type=float, default=1e-4)      # option that takes a value
 
@@ -47,23 +49,23 @@ if __name__ == "__main__":
     # model_type = "vit_h" # ViT-h seems to work best
     # sam = sam_model_registry[model_type](checkpoint="/mnt/shared/lswezel/weights/sam_vit_h_4b8939.pth") # replace with your own path
     model_type = "vit_b" # ViT-h seems to work best
-    sam = sam_model_registry[model_type](checkpoint="/mnt/shared/lswezel/weights/sam_vit_b_01ec64.pth") # replace with your own path
+    sam = sam_model_registry[model_type](checkpoint=f"{args.checkpoint_path}/sam_vit_b_01ec64.pth") # replace with your own path
     # alternative backbones
     efficientnet = IntermediateLayerGetter(torchvision.models.efficientnet_b0(pretrained=True).features, return_layers={"5":"layer5"}).to(device=device).to(PRECISION).eval()
 
 
 
-    if os.path.exists("sam.pth"):
+    if os.path.exists(f"{args.checkpoint_path}/{args.checkpoint_loadname}"):
         print("Loading checkpoint from file...")
-        sam.load_state_dict(torch.load("sam.pth"))
+        sam.load_state_dict(torch.load(f"{args.checkpoint_path}/{args.checkpoint_loadname}"))
 
 
     sam.to(device=device).to(PRECISION)
     sam.prompt_encoder.pe_layer.precision = PRECISION
 
 
-    optimizer = torch.optim.Adam(sam.mask_decoder.parameters(), lr=1e-5, weight_decay=0)
-    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+    optimizer = torch.optim.Adam(sam.mask_decoder.parameters(), lr=1e-4, weight_decay=0)
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
     # grad scaler for mixed precision training
     scaler = torch.cuda.amp.GradScaler(enabled=True)
 
@@ -74,42 +76,67 @@ if __name__ == "__main__":
 
     preprocess_transform = transform = ResizeLongestSide(sam.image_encoder.img_size)
 
-    augmentations = A.Compose([
-            # Spatial augmentations
-            A.HorizontalFlip(p=0.2),
-            # A.augmentations.geometric.rotate.SafeRotate(limit=20),
-            A.VerticalFlip(p=0.2),
-            # Texture augmentations
-            # A.RandomBrightnessContrast(p=0.2),
-            # A.GaussianBlur(p=0.1),
-            # A.CLAHE(p=0.2),
-            # A.ColorJitter(p=0.1),
-            # A.ChannelDropout(p=0.1),
-        ])
+    # augmentations = A.Compose([
+    #         # Spatial augmentations
+    #         A.HorizontalFlip(p=0.2),
+    #         # A.augmentations.geometric.rotate.SafeRotate(limit=20),
+    #         A.VerticalFlip(p=0.2),
+    #         # Texture augmentations
+    #         # A.RandomBrightnessContrast(p=0.2),
+    #         # A.GaussianBlur(p=0.1),
+    #         # A.CLAHE(p=0.2),
+    #         # A.ColorJitter(p=0.1),
+    #         # A.ChannelDropout(p=0.1),
+    #     ])
 
 
-    train_dataset = PromptableMetaDataset([
-                                    'malignant',
-                                    # 'TCGA_CS_4941_19960909',
-                                    # 'TCGA_CS_4942_19970222',
-                                    'TCGA_CS_4943_20000902',
-                                    # 'TCGA_CS_4944_20010208',
-                                    # 'TCGA_CS_5393_19990606',
-                                    # 'TCGA_CS_5395_19981004',
-                                    'TCGA_CS_5396_20010302',
-                                    'TCGA_CS_5397_20010315',
-                                    'TCGA_CS_6186_20000601',
-                                    'TCGA_CS_6188_20010812',
-                                    'TCGA_CS_6290_20000917',
+    train_dataset = PromptableMetaDataset(
+                                "/mnt/qb/work/akata/aoq833/sam_finetuning_data",
+                                [
+                                'malignant',
+                                'TCGA_CS_4941_19960909',
+                                'TCGA_CS_4942_19970222',
+                                'TCGA_CS_4943_20000902',
+                                'TCGA_CS_4944_20010208',
+                                'TCGA_CS_5393_19990606',
+                                'TCGA_CS_5395_19981004',
+                                'TCGA_CS_5396_20010302',
+                                'TCGA_CS_5397_20010315',
+                                'TCGA_CS_6186_20000601',
+                                'TCGA_CS_6188_20010812',
+                                'TCGA_CS_6290_20000917',
+                                'TCGA_CS_6665_20010817',
+                                'TCGA_CS_6666_20011109',
+                                'TCGA_CS_6667_20011105',
+                                'TCGA_CS_6668_20011025',
+                                'TCGA_CS_6669_20020102',
+                                'TCGA_DU_5849_19950405',
+                                'TCGA_DU_5851_19950428',
+                                'TCGA_DU_5852_19950709',
+                                'TCGA_DU_5853_19950823',
+                                'TCGA_DU_5854_19951104',
+                                'TCGA_DU_5855_19951217',
+                                'TCGA_DU_5871_19941206',
+                                'TCGA_DU_5872_19950223',
+                                'TCGA_DU_5874_19950510',
+                                'TCGA_DU_6399_19830416',
+                                'TCGA_DU_6400_19830518',
+                                'TCGA_DU_6401_19831001',
+                                'TCGA_DU_6404_19850629',
+                                'TCGA_DU_6405_19851005',
+                                'TCGA_DU_6407_19860514',
+                                'TCGA_DU_6408_19860521',
                                     ],
-                                    transforms=augmentations,
+                                    # transforms=augmentations,
                                     precision=PRECISION,
                                 )
     print(f"Training sample size: {len(train_dataset)}")
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=4, drop_last=True) 
 
 
-    test_dataset = PromptableMetaDataset([
+    test_dataset = PromptableMetaDataset(
+                                        "/mnt/qb/work/akata/aoq833/sam_finetuning_data",
+                                    [
                                     'benign',
                                     'TCGA_HT_A61B_19991127',
                                     ],
@@ -143,8 +170,8 @@ if __name__ == "__main__":
             with torch.no_grad():
 
                 # encode image
-                # image_embedding = sam.image_encoder(input_image)
-                image_embedding = efficientnet(input_image)["layer5"]
+                image_embedding = sam.image_encoder(input_image)
+                # image_embedding = efficientnet(input_image)["layer5"]
                 image_embedding = F.pad(image_embedding, (0, 0, 0, 0, 0, 256 - image_embedding.shape[1])) # pad along channel dimension
 
                 # encode prompt
@@ -210,8 +237,8 @@ if __name__ == "__main__":
             with torch.no_grad():
 
                 # encode image
-                # image_embedding = sam.image_encoder(input_image)
-                image_embedding = efficientnet(input_image)["layer5"]
+                image_embedding = sam.image_encoder(input_image)
+                # image_embedding = efficientnet(input_image)["layer5"]
                 image_embedding = F.pad(image_embedding, (0, 0, 0, 0, 0, 256 - image_embedding.shape[1])) # pad along channel dimension
 
                 # encode prompt
@@ -251,5 +278,6 @@ if __name__ == "__main__":
         # save checkpoint
         if results["jaccard_score"] > best_jaccard:
             best_jaccard = results["jaccard_score"]
-            torch.save(sam.state_dict(), f"sam2.pth")
+            print(f"New best jaccard score: {best_jaccard:.4f}")
+            torch.save(sam.state_dict(), f"{args.checkpoint_path}/{args.checkpoint_savename}")
         
--- finetune_mask_decoder.sh
+++ finetune_mask_decoder.sh
@@ -2,12 +2,12 @@
 #SBATCH --ntasks=1                 # Number of tasks (see below)
 #SBATCH --cpus-per-task=4          # Number of CPU cores per task
 #SBATCH --nodes=1                  # Ensure that all cores are on one machine
-#SBATCH --time=0-00:10             # Runtime in D-HH:MM
+#SBATCH --time=0-40:00             # Runtime in D-HH:MM
 #SBATCH --partition gpu-2080ti
 #SBATCH --gres=gpu:1               # optionally type and number of gpus
-#SBATCH --mem=50G                  # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH --output=/mnt/qb/work/akata/aoq833/logs/hostname_%j.out   # File to which STDOUT will be written - make sure this is not on $HOME
-#SBATCH --error=/mnt/qb/work/akata/aoq833/logs/hostname_%j.err    # File to which STDERR will be written - make sure this is not on $HOME
+#SBATCH --mem=100G                  # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --output=/mnt/qb/work/akata/aoq833/sam_logs/hostname_%j.out   # File to which STDOUT will be written - make sure this is not on $HOME
+#SBATCH --error=/mnt/qb/work/akata/aoq833/sam_logs/hostname_%j.err    # File to which STDERR will be written - make sure this is not on $HOME
 #SBATCH --mail-type=ALL            # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=stefan.wezel@maddox.ai   # Email to which notifications will be sent
 
@@ -25,7 +25,7 @@ source /home/akata/aoq833/ad/bin/activate
 
 
 
-python finetune_mask_decoder.py 
+python finetune_mask_decoder.py --lr 0.0001 --checkpoint_loadname sam_full.pth --checkpoint_savename sam_full2.pth
 
 
 
