c1771b96613b6d26dd3ef2d1aba5e6012bcbfe70
Luca Alfonzo <94303649+LucaA18@users.noreply.github.com>
2023-07-10 23:40:11 +0200

    add run_bash.py to run inference locally

---
+++ run_bash.py
@@ -0,0 +1,6 @@
+import subprocess
+
+print("Inference script executed successfully!")
+subprocess.call("C:/Users/lucaa/Desktop/Master Studium/SS23/Praktikum Machine Learning in Graphics, Vision and Language (MSc) - SoSe 23/MedicalImageSegmentation WebDev/run_inference.sh", shell=True)
+# You can uncomment the following line if you want to redirect to a URL
+# print("Location: http://127.0.0.1:8000/annotation?success=true")
