5b0c8ba8e9413496b4c33b362db2d862b6c1ee33
Luca Alfonzo <94303649+LucaA18@users.noreply.github.com>
2023-07-10 23:52:36 +0200

    add run_inference.sh

---
+++ run_inference.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python onnx_inference.py --onnx-checkpoint "C:\Users\lucaa\Desktop\Master Studium\SS23\Praktikum Machine Learning in Graphics, Vision and Language (MSc) - SoSe 23\sam_finetuned.onnx" --input_df example_user_input.csv
