2ec798b932eb3d17f35b02edefdcb06d8ab904a1
Daniel Norbert Meister <d.meister@ncl.ac.uk>
2023-08-16 19:18:41 +0200

    Completed and removed todos from various locations. Removed prints, console logs etc.

--- frontendPrototype/prototypeSite/prototypeSite/urls.py
+++ frontendPrototype/prototypeSite/prototypeSite/urls.py
@@ -19,7 +19,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-# TODO: fix import error
 from prototypeApp import views
 
 urlpatterns = [
